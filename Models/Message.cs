c4c1a2d2bb197a04499f3de31bc542e7ccb5a597
ABBAS ALMUKHTAFI <abbasyemen2@gmail.com>
2026-04-25 19:18:30 +0300

    Refactor Message class properties for nullability
    Updated properties in Message class to use default values and nullability.

--- Models/Message.cs
+++ Models/Message.cs
@@ -2,21 +2,18 @@
 
 namespace SecureCommunicationApp.Models
 {
-    public class Message
-    {
-        public int MessageId { get; set; }
+   public class Message
+{
+    public int MessageId { get; set; }
 
-        [Required]
-        public string Content { get; set; }
+    public string Content { get; set; } = string.Empty;
 
-        public DateTime SentDate { get; set; } = DateTime.Now;
+    public DateTime SentDate { get; set; }
 
-        // Sender
-        public int SenderId { get; set; }
-        public User Sender { get; set; }
+    public int SenderId { get; set; }
+    public User Sender { get; set; } = null!;
 
-        // Receiver
-        public int ReceiverId { get; set; }
-        public User Receiver { get; set; }
-    }
+    public int ReceiverId { get; set; }
+    public User Receiver { get; set; } = null!;
+}
 }
