1192f1badd8f0aad547669ce07c13392657116d0
ABBAS ALMUKHTAFI <abbasyemen2@gmail.com>
2026-04-25 19:16:13 +0300

    Refactor Program.cs for cleaner structure

--- Program.cs
+++ Program.cs
@@ -1,34 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using SecureCommunicationApp.Data;
 
-namespace SecureCommunicationApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services
-            builder.Services.AddControllersWithViews();
-            
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlite("Data Source=app.db"));
-            
-            var app = builder.Build();
-            
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            
-            app.UseRouting();
-            
-            app.UseAuthorization();
-            
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            
-            app.Run();
-        }
-    }
-}
+// MVC
+builder.Services.AddControllersWithViews();
+
+// DB (SQLite)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
